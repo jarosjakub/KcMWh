77a0066b9836e3c09b302a92d7b15aae9e01e62d
Jakub Jaroš <jarosj2@medtronic.com>
2024-08-02 13:20:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace KcMWh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:\\Users\\jakub.jaros\\Desktop\\kcmwh.txt";
+            var OutputPath = "C:\\Users\\jakub.jaros\\Desktop\\outputDeml.txt";
+
+            var content = File.ReadAllText(path);
+            string[] line = content.Split("\r\n");
+            var lenght = line.Length;
+            Console.WriteLine(lenght);
+
+            for (int i = 0;i < lenght; i++) 
+            {
+                using (StreamWriter outputFile = new StreamWriter(OutputPath, true))
+                {
+                    outputFile.WriteLine(line[i]);
+                    outputFile.WriteLine(line[i]);
+                    outputFile.WriteLine(line[i]);
+                    outputFile.WriteLine(line[i]);
+
+                    //outputFile.WriteLine(i + "\t" + line[i]);
+                    //outputFile.WriteLine(i + "\t" + line[i]);
+                    //outputFile.WriteLine(i + "\t" + line[i]);
+                    //outputFile.WriteLine(i + "\t" + line[i]);
+                }
+            }
+        }
+    }
+}
