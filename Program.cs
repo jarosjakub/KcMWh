acc9cd7e90e76c86f6ce0a1072449fcad9413840
Jakub Jaroš <jakub.s.jaros@gmail.com>
2025-01-25 10:55:27 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -6,24 +6,25 @@ namespace KcMWh
     {
         static void Main(string[] args)
         {
-            var path = "C:\\Users\\jakub.jaros\\Desktop\\kcmwh.txt";
-            var OutputPath = "C:\\Users\\jakub.jaros\\Desktop\\outputDeml.txt";
+            var path = "C:\\Users\\jakub\\Desktop\\Deml\\kcmwh.txt";
+            var OutputPath = "C:\\Users\\jakub\\Desktop\\Deml\\outputDeml6.txt";
 
             var content = File.ReadAllText(path);
             string[] line = content.Split("\r\n");
             var lenght = line.Length;
             Console.WriteLine(lenght);
-            var j = 0;
+            var j = 1;
+            var d = 1;
 
             for (int i = 0; i < lenght; i++)
             {
                 
                 using (StreamWriter outputFile = new StreamWriter(OutputPath, true))
                 {
-                    outputFile.WriteLine(j + "\t" + line[i]);
-                    outputFile.WriteLine(j + "\t" + line[i]);
-                    outputFile.WriteLine(j + "\t" + line[i]);
-                    outputFile.WriteLine(j + "\t" + line[i]);
+                    outputFile.WriteLine(d + "\t" + j + "\t" + line[i]);
+                    outputFile.WriteLine(d + "\t" + j + "\t" + line[i]);
+                    outputFile.WriteLine(d + "\t" + j + "\t" + line[i]);
+                    outputFile.WriteLine(d + "\t" + j + "\t" + line[i]);
                 }
                 if (j < 24)
                 {
@@ -32,6 +33,7 @@ namespace KcMWh
                 else
                 {
                     j = j - 23;
+                    d++;
                 }
             }
         }
